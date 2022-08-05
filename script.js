8a2178637b94b8870c8a69f2744255eb6228d3a9
Nelson Luiz <110398507+Nelsoff1@users.noreply.github.com>
2022-08-05 12:28:04 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function bigbang(){
+  alert('20 anos e estou cursando Engenharia de Software')
+  }
+function inicio(){
+  alert('Motorista de caminhao, Manutenção de empresa(pintura, solda, lixa, restauração e concertos nos maquinarios), operador de maquina, ajudante de pedreiro e ajudante de motorista')
+  }
+function meio(){
+  alert('Curso para mecânico de moto(smart),Certificação da NR-35(trabalho em altura), dois cursos presenciais em daytrade(erudição investimentos), um online(TORO investimentos) e o do mil ao milhão 3.0(Primo Rico)')
+  }
+function fim(){
+  alert('Handebol, motocross, futebol, parkour, basquete, corrida em montanha, muay thai e downhill ')
+ } 
+
+  
